708de80c05cc45c2625a6265927d3481050725e1
shamsheev <svr93@i.ua>
2016-07-10 13:43:50 +0300

    add String.fromCodePoint polyfill

--- base.sh
+++ base.sh
@@ -5,3 +5,4 @@ bower i --save es6-math
 bower i --save array-includes
 bower i --save String.prototype.includes
 bower i --save git://github.com/es-shims/String.prototype.codePointAt.git
+bower i --save git://github.com/es-shims/String.fromCodePoint.git
