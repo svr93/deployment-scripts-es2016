8a6b4e0f299a86c991694a3782550457a64fd379
shamsheev <svr93@i.ua>
2016-07-16 20:34:18 +0300

    add Proxy polyfill

--- base.sh
+++ base.sh
@@ -10,3 +10,4 @@ bower i --save array-includes
 bower i --save String.prototype.includes
 bower i --save git://github.com/es-shims/String.prototype.codePointAt.git
 bower i --save git://github.com/es-shims/String.fromCodePoint.git
+bower i --save proxy-polyfill
